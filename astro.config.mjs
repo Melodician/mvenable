bf123026b6881049e4061af96ee7e8b542dd2521
Mat Venable <compositional@gmail.com>
2023-10-04 20:14:20 -0700

    preact, greeting, view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mvenable.vercel.app"
+  site: "https://mvenable.vercel.app",
+  integrations: [preact()]
 });
