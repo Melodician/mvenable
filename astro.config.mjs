9301e86d0f2e366eecd2c3120b94100eaf3d1270
Melodician <compositional@gmail.com>
2023-11-28 20:54:23 -0800

    MDX fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
-import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mvenable.vercel.app",
-  integrations: [preact(), mdx()]
+  integrations: [preact()]
 });
